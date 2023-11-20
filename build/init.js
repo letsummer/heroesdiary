16942a6e81f63a861084f3571adbee3f78ab49cc
letsummer <letsummerparty@gmail.com>
2023-11-20 19:39:09 +0900

    add process.env.PORT

--- build/init.js
+++ build/init.js
@@ -5,7 +5,7 @@ require("./db.js");
 require("./models/Diary.js");
 var _index = _interopRequireDefault(require("./index.js"));
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
-var PORT = 4000;
+var PORT = process.env.PORT || 4000;
 _index["default"].listen(PORT, function () {
   return console.log("Listening!");
 });
--- src/init.js
+++ src/init.js
@@ -2,7 +2,7 @@ import "dotenv/config";
 import "./db.js";
 import "./models/Diary.js";
 import app from "./index.js";
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, ()=>console.log(`Listening!`));
 
